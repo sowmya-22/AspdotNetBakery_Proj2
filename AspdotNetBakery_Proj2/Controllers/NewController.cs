60bb1d651ae8f2cc085748b9df71076d698b36df
YasaswiniLella <yasaswini.lella@gmail.com>
2020-09-28 15:04:21 +0530

    Created newcontroller

---
+++ AspdotNetBakery_Proj2/Controllers/NewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspdotNetBakery_Proj2.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
