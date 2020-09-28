eaac7468ea07c7877fcac10742137fba6e0dc663
KOTHWALGUDEM <sowmya.kothwalgudem@dxc.com>
2020-09-28 11:04:01 +0530

    Add project files.

---
+++ AspdotNetBakery_Proj2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspdotNetBakery_Proj2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
